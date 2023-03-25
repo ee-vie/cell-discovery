364d346c35e777748c2e3acf58015ab52597c6c7
Dangobert <121398477+Dangobert@users.noreply.github.com>
2023-03-25 21:38:58 +0100

    Description in CellScence
    Description aufrufen funktioniert noch nicht, aber man kann die Objekte anklicken

--- Assets/LaserPointer.gd
+++ Assets/LaserPointer.gd
@@ -2,10 +2,16 @@ extends Spatial
 
 var distance
 var target
+onready var rowner = get_owner()
+onready var rcontroller = rowner.get_node("RightHandController") 
+onready var cam = rowner.get_node("ARVRCamera")
+#var rcontroller = get_owner().get_node("RightHandController")
 
+#func _ready():
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if $RayCast.get_collider():
 		distance = transform.origin.distance_to($RayCast.get_collision_point())
 		$Scaler.scale.z = distance /2
@@ -16,12 +22,15 @@ func _process(delta):
 		target = $RayCast.get_collider()
 		if target.has_method("hide_object"):
 			target.hide_object(false)
+			if rcontroller.is_button_pressed(15):
+				var descr = target.show_description(target)
+				rowner.add_child(descr)
+				
 	else:
 		if target == null or !target.has_method("hide_object"):
 			pass
 		else:
 			target.hide_object(true)
-			
 #	if $RayCast.is_colliding():
 #		var point = $RayCast.get_collision_point()
 #
