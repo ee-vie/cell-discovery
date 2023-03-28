d89676c2c3cac0ee9058efa595ab26fb634ffc7c
Dangobert <121398477+Dangobert@users.noreply.github.com>
2023-03-28 22:31:14 +0200

    TranskriptionGame
    Added all Features to the game

---
+++ Assets/BaseCheckUracil.gd
@@ -0,0 +1,19 @@
+extends Area
+
+
+# Number of Base to check where to place after drop
+var checknumber = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+#Used to check if its the right base, area with collision needed, because picking up a Object disables the main collision shape?!
+func Base1():
+	pass
+
+func Uracile():
+	pass
+
+func get_checknumber():
+	return checknumber
