a0684cb6157f816e56d07dac45d007c8fbd2df81
ee-vie <128611555+ee-vie@users.noreply.github.com>
2023-03-30 01:38:14 +0200

    button test

---
+++ addons/gui_in_vr/gui_panel_3d_menu.gd
@@ -0,0 +1,19 @@
+extends StaticBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if lcontroller.is_button_pressed(1): #Y button
+		get_tree().change_scene("res://Scences/CellMainScence.tscn")
+	if lcontroller.is_button_pressed(7): #X Button
+		get_tree().change_scene("res://Scences/TranskriptionGame.tscn")
