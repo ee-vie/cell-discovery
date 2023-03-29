8bb64c39a9fbce7eec4283722e42561953237fec
ee-vie <128611555+ee-vie@users.noreply.github.com>
2023-03-29 15:35:43 +0200

    Menu and Splash screen added

---
+++ addons/gui_in_vr/mainButtons/MiniGamesButton.gd
@@ -0,0 +1,4 @@
+extends Button
+
+export (String, FILE, '*.tscn') var scene_to_load : String = ("res://Scences/TranskriptionGame.tscn")
+
---
+++ addons/gui_in_vr/subMenu/return_to_title.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://addons/gui_in_vr/gui_klara.tscn")
