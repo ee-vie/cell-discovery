b71227084e5354e2120bb50087b594a8802b2d1d
ee-vie <128611555+ee-vie@users.noreply.github.com>
2023-03-30 02:36:16 +0200

    works (sort of)

--- addons/gui_in_vr/gui_panel_3d_menu.gd
+++ addons/gui_in_vr/gui_panel_3d_menu.gd
@@ -4,7 +4,8 @@ extends StaticBody
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+onready var rowner = get_owner()
+onready var lcontroller = rowner.get_node("LeftHandController") 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
